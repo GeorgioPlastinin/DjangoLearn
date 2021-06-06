702d254d702039e3df47ccfef0f1081fa43ef13c
Georgiy <georgy.plastinin@yandex.ru>
2021-06-06 17:47:29 +0300

    dz3.1
    dz3.1

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from products import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    path('products/', views.products, name='products'),
+    path('products/', include('products.urls', namespace='products')),
 ]
 
 if settings.DEBUG:

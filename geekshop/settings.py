1a45671ed8c6525d91c9e7676f6b8679ce6c83b1
tcampert <tcampert@yandex.ru>
2021-06-13 13:54:12 +0300

    dz4

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'products',
     'users',
+    'baskets'
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
+
+LOGIN_URL = '/users/login/'
